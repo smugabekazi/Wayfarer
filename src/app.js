8f2ec556f1da85dcfd9cb6126b1bd09fa2313682
schella mugabekazi <schellamugabekazi762@gmail.com>
2019-08-06 14:01:08 +0200

    Feature(create trip): create trip endpoint
    -   endpoint for creating a trip
    - adding a JS file for trip in routes,helpers,and controllers
    
    [Finishes #167691965]

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import userRouter from './routes/users';
-
+import tripRouter from './routes/trips';
 const app = express();
-
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
   }),
 );
-
 app.use('/api/v1/auth', userRouter);
+app.use('/api/v1/trip', tripRouter);
 export default app;
---
+++ src/controllers/trips.js
@@ -0,0 +1,42 @@
+import trips from '../models/trips';
+class Trips {
+  /* Checking if the Trip exixts */
+  static checkTrip(tripId) {
+    let checkTrip = {};
+    for (const key in trips) {
+      if (trips[key].id === tripId) {
+        checkTrip = trips[key];
+        break;
+      }
+    }
+return checkTrip;
+  }
+  /* create */
+  static create(req, res) {
+    const newTrip = {
+      id: Math.ceil(Math.random() * 100),
+      createdOn: Date.now(),
+      seatingCapacity: req.body.seatingCapacity,
+      busLicenseNumber: req.body.busLicenseNumber,
+      origin: req.body.origin,
+      destination: req.body.destination,
+      tripDate: Date.parse(new Date(req.body.tripDate)),
+      fare: req.body.fare,
+      status: req.body.status,
+    };
+    trips.push(newTrip);
+    const isCreated = Trips.checkTrip(newTrip.id);
+    if (Object.keys(isCreated).length > 0) {
+      return res.status(201).json({
+        status: 201,
+        message: 'trip created successfully',
+        data: isCreated,
+      });
+    }
+    return res.status(400).json({
+      status: 400,
+      error: 'Trip not created!',
+    });
+  }
+}
+export default Trips;
